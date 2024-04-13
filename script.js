f2ece784c10fc28f0cd6b79a6644f9dc9b8c26a0
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-04-13 19:50:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+let age=document.getElementById("age");
+let name1=document.getElementById("name");
+const promise1=()=>{ return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+    if(parseInt(age.value)>=18){
+        resolve(`welcome, ${name1.value}. You can vote.`)
+    }
+    else reject(`Oh sorry ${name1.value}. You aren't old enough.`)
+    },4000)
+});
+}
+
+ function solve(event){
+    event.preventDefault();
+    
+    promise1().then((data1)=>{
+        console.log("this is done")
+    console.log(promise1());
+        alert(data1)
+    }).catch((err)=>{
+        alert(err);
+    })
+ }
+document.getElementById("btn").addEventListener("click",solve);
